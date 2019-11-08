dc5ec26d35ecdfca21292a6c998fab00ba2c6963
Prakash Joshi <prakashjoshi0101@gmail.com>
2019-11-08 08:38:54 -0800

    Setup view binding

--- app/src/main/java/com/prakashj/xkcd/home/MainActivity.kt
+++ app/src/main/java/com/prakashj/xkcd/home/MainActivity.kt
@@ -2,20 +2,32 @@ package com.prakashj.xkcd.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.prakashj.xkcd.databinding.ActivityMainBinding
 import com.prakashj.xkcd.infra.di.AppViewModelFactory
-import com.prakashj.xkcd.R
+import com.prakashj.xkcd.infra.network.Comic
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var viewModel: MainViewModel
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        viewModel = ViewModelProvider(this,
-            AppViewModelFactory()
-        ).get(MainViewModel::class.java)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this, AppViewModelFactory()).get(MainViewModel::class.java)
+
+        setupLiveData(viewModel)
+    }
+
+    private fun setupLiveData(viewModel: MainViewModel) {
+        viewModel.comicLiveData.observe(this, Observer<Comic> { comic: Comic ->
+            binding.titleTextView.text = comic.title
+            binding.alternateTextView.text = comic.alt
+        })
     }
 }
--- app/src/main/java/com/prakashj/xkcd/home/MainViewModel.kt
+++ app/src/main/java/com/prakashj/xkcd/home/MainViewModel.kt
@@ -1,14 +1,19 @@
 package com.prakashj.xkcd.home
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.prakashj.xkcd.infra.network.ApiService
+import com.prakashj.xkcd.infra.network.Comic
 import kotlinx.coroutines.launch
+import retrofit2.Response
+
+private const val TAG = "MainViewModel"
 
 class MainViewModel(private val apiService: ApiService) : ViewModel() {
 
-    val TAG = "MainViewModel"
+    val comicLiveData: MutableLiveData<Comic> = MutableLiveData<Comic>()
 
     init {
         getCurrentComic()
@@ -16,11 +21,17 @@ class MainViewModel(private val apiService: ApiService) : ViewModel() {
 
     private fun getCurrentComic() {
         viewModelScope.launch {
-            val comicResponse = apiService.getCurrentComic()
+            val comicResponse: Response<Comic> = apiService.getCurrentComic()
 
             Log.d(TAG, comicResponse.toString())
-
             Log.d(TAG, comicResponse.body().toString())
+
+            if (comicResponse.isSuccessful) {
+
+                comicResponse.body()?.apply {
+                    comicLiveData.postValue(comicResponse.body())
+                }
+            }
         }
     }
 }
